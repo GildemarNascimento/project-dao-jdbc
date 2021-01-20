ebe098a5594dd2b4d4e1a802e6f6fabd4495701c
GildemarNascimento <gildemargg@hotmail.com>
2021-01-19 22:25:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,10 @@ public class Program {
 		
 		Department department = new Department(1,  "Books");
 		
+		Seller seller = new Seller(21, "Boby","Boby@gmail.com", new Date(), 3000.00, department);
+		
 		System.out.println(department);
+		System.out.println(seller);
 
 	}
 
